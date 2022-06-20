3c863b931d28ccbd9e8e498b603c9e933aa85b8b
asafedainez <asafe.sousa@gmail.com>
2022-06-20 17:26:31 -0300

    feat(middleware): implementando middleware de verificação de token JWT

---
+++ src/middleware/jwtToken.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('../utils/jwt');
+const httpStatus = require('../utils/http');
+
+  const tokenMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(httpStatus.UNAUTHORIZED).json({
+        status: httpStatus.UNAUTHORIZED,
+        message: 'Token not found',
+      });
+    }
+
+    await jwt.verifyToken(token);
+
+    next();
+  };
+
+  module.exports = tokenMiddleware;
