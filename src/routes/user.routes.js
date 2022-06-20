3ea369a1005ac26a98a8f95c83dde3155be38f5f
asafedainez <asafe.sousa@gmail.com>
2022-06-20 17:29:04 -0300

    feat(rotas): implementado endpoint GET em /user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,9 @@
 const Routes = require('express').Router();
 const userMiddleware = require('../middleware/user.middleware');
+const jwtTokenMiddleware = require('../middleware/jwtToken.middleware');
 const controller = require('../controllers/user.controller');
 
+Routes.get('/', jwtTokenMiddleware, controller.getAll);
 Routes.post('/', userMiddleware, controller.insertUser);
 
 module.exports = Routes;
