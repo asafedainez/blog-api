16508cba382eb5bc23ec99a0cd25e10ce20b4452
asafedainez <asafe.sousa@gmail.com>
2022-06-20 16:33:02 -0300

    feat(controller): implementado método insertUser na controller de POST /user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,19 @@
-// const httpStatus = require('../utils/http');
+const httpStatus = require('../utils/http');
+const service = require('../services/user.service');
+
+const insertUser = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+
+  const token = await service.create({
+    displayName,
+    email,
+    password,
+    image,
+  });
+
+  return res.status(httpStatus.CREATED).json({ token });
+};
+
+module.exports = {
+  insertUser,
+};
