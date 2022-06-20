8001381a9816e79d4238609afdecaf8e659fc196
asafedainez <asafe.sousa@gmail.com>
2022-06-20 16:33:42 -0300

    feat(services): implementado método create na service de POST /user

---
+++ src/services/user.service.js
@@ -0,0 +1,23 @@
+const { User } = require('../database/models');
+const { generateToken } = require('../utils/jwt');
+const httpStatus = require('../utils/http');
+
+const create = async (data) => {
+  const userFound = await User.findOne({ where: { email: data.email } });
+
+  if (userFound) {
+    const error = {
+      status: httpStatus.CONFLICT,
+      message: 'User already registered',
+    };
+  throw error;
+  }
+  
+  const user = await User.create(data);
+  const { id, email } = user;
+  return generateToken({ id, email });
+};
+
+module.exports = {
+  create,
+};
