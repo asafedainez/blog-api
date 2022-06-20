cab922cb909138fb76ab8b8943cc55662db6a34c
asafedainez <asafe.sousa@gmail.com>
2022-06-20 17:52:22 -0300

    feat(sequelize model): adicionando coluna id

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -2,6 +2,12 @@ const CategorySchema = (sequelize, DataTypes) => {
   const CategoryTable = sequelize.define(
     'Category',
     {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
       name: DataTypes.STRING,
     },
     {
