d57254b16dd37ed725158d660423e3fc0f4b8baf
asafedainez <asafe.sousa@gmail.com>
2022-06-20 22:20:36 -0300

    feat(rotas): implementado endpoint GET para /post

--- src/routes/blogPost.routes.js
+++ src/routes/blogPost.routes.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/blogPost.controller');
 const jwtTokenMiddleware = require('../middleware/jwtToken.middleware');
 const blogPostMiddleware = require('../middleware/blogPost.middleware');
 
-// Routes.get('/', controller.getAll);
+Routes.get('/', jwtTokenMiddleware, controller.getAll);
 Routes.post('/', jwtTokenMiddleware, blogPostMiddleware, controller.create);
 
 module.exports = Routes;
