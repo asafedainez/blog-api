3ce109117aa667019ae251fafc45dffd58e76b0d
asafedainez <asafe.sousa@gmail.com>
2022-06-21 16:55:13 -0300

    feat(rotas): implementado endpoint PUT /post/:id para atualização de post

--- src/routes/blogPost.routes.js
+++ src/routes/blogPost.routes.js
@@ -1,10 +1,14 @@
 const Routes = require('express').Router();
 const controller = require('../controllers/blogPost.controller');
 const jwtTokenMiddleware = require('../middleware/jwtToken.middleware');
-const blogPostMiddleware = require('../middleware/blogPost.middleware');
+const {
+  blogPostMiddleware,
+  blogPostUpdateMiddleware,
+} = require('../middleware/blogPost.middleware');
 
 Routes.get('/', jwtTokenMiddleware, controller.getAll);
 Routes.get('/:id', jwtTokenMiddleware, controller.getById);
 Routes.post('/', jwtTokenMiddleware, blogPostMiddleware, controller.create);
+Routes.put('/:id', jwtTokenMiddleware, blogPostUpdateMiddleware, controller.update);
 
 module.exports = Routes;
