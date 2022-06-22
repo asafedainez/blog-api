0c04d8f7cd4d02e78c4d5c13845825b3df41d375
asafedainez <asafe.sousa@gmail.com>
2022-06-22 17:01:27 -0300

    feat(rotas): implementado endpoint de DELETE na rota /user/me

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,5 +6,6 @@ const controller = require('../controllers/user.controller');
 Routes.get('/:id', jwtTokenMiddleware, controller.getUser);
 Routes.get('/', jwtTokenMiddleware, controller.getAll);
 Routes.post('/', userMiddleware, controller.insertUser);
+Routes.delete('/me', jwtTokenMiddleware, controller.deleteUser);
 
 module.exports = Routes;
